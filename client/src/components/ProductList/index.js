25fd64a5f6399687e97ed9918d0445525e339e5f
PSatt <parkers205@gmail.com>
2021-11-24 16:41:08 -0600

    converted components to redux via usedispatch and use selector

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -6,9 +6,12 @@ import { useQuery } from '@apollo/client';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 import spinner from '../../assets/spinner.gif';
+import { useDispatch, useSelector } from 'react-redux';
 
 function ProductList() {
-  const [state, dispatch] = useStoreContext();
+  // const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
 
   const { currentCategory } = state;
 
